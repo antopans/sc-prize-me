90874af3b15ba042ab9305362f280fbfd92ee034
antopans <antopans@gmail.com>
2022-03-21 00:38:22 +0100

    Clean tree structure

---
+++ interaction/test_wallets.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+OWNER="./interaction/wallets/test/sc-owner.pem"
+
+SPONSOR1="./interaction/wallets/test/sponsor1.pem"
+SPONSOR2="./interaction/wallets/test/sponsor2.pem"
+SPONSOR3="./interaction/wallets/test/sponsor3.pem"
+SPONSOR4="./interaction/wallets/test/sponsor4.pem"
+
+PLAYER1="./interaction/wallets/test/player1.pem"
+PLAYER2="./interaction/wallets/test/player2.pem"
+PLAYER3="./interaction/wallets/test/player3.pem"
+PLAYER4="./interaction/wallets/test/player4.pem"
+PLAYER5="./interaction/wallets/test/player5.pem"
+PLAYER6="./interaction/wallets/test/player6.pem"
+
+ADDR_ZERO="0x0000000000000000000000000000000000000000000000000000000000000000"
---
+++ wasm/src/lib.rs
@@ -0,0 +1,52 @@
+////////////////////////////////////////////////////
+////////////////// AUTO-GENERATED //////////////////
+////////////////////////////////////////////////////
+
+#![no_std]
+
+elrond_wasm_node::wasm_endpoints! {
+    prize
+    (
+        addAddrBlacklist
+        claimDonations
+        claimFees
+        claimPrize
+        cleanClaimed
+        create
+        disable
+        getAddrBlacklist
+        getAllInfo
+        getAllInfoFrag
+        getCharityPool
+        getFeePol
+        getFeePool
+        getIDs
+        getInfo
+        getLogEnableStatus
+        getNb
+        getNbSponsorRunning
+        getParamDuration
+        getParamManClaim
+        getParamNbMaxInstancesPerSponsor
+        getParamSponsorInfoMaxLength
+        getPlayerIDs
+        getRemainingTime
+        getSponsorIDs
+        getStatus
+        hasPlayed
+        hasStatus
+        hasWon
+        play
+        prize
+        rmAddrBlacklist
+        setFeePol
+        setLogEnableStatus
+        setParamDuration
+        setParamManClaim
+        setParamNbMaxInstancesPerSponsor
+        setParamSponsorInfoMaxLength
+        setPremium
+    )
+}
+
+elrond_wasm_node::wasm_empty_callback! {}
